60556109d18d3ee6b123c3bf9798bac89b3497f6
LUOHONGPENG <32034557+LUOHONGPENG@users.noreply.github.com>
2023-02-07 15:29:14 +0000

    FirstInsFinish

--- Assets/Scripts/BigLevels/LevelFirstIns.cs
+++ Assets/Scripts/BigLevels/LevelFirstIns.cs
@@ -8,6 +8,7 @@ public class LevelFirstIns : LevelBasic
 {
     public Image imgLike;
     public Button btnLike;
+    public Text codeLike;
     public ParticleSystem particleLike;
 
     public GameObject pfComment;
@@ -15,12 +16,16 @@ public class LevelFirstIns : LevelBasic
 
 
     private bool isLike = false;
+    private int numLike = 0;
 
     public override void Init(LevelManager parent)
     {
         base.Init(parent);
 
+        //Init view
         imgLike.DOFade(0.01f, 0);
+        codeLike.text = 0.ToString();
+        numLike = 0;
 
         btnLike.onClick.RemoveAllListeners();
         btnLike.onClick.AddListener(delegate ()
@@ -28,6 +33,7 @@ public class LevelFirstIns : LevelBasic
             if (!isLike)
             {
                 StartCoroutine(IE_FirstLike());
+                StartCoroutine(IE_FirstLikeNum());
                 isLike = true;
             }
         });
@@ -37,14 +43,41 @@ public class LevelFirstIns : LevelBasic
     {
         imgLike.DOFade(1f, 0.5f);
         imgLike.transform.DOScale(2f, 0.5f);
+        
         yield return new WaitForSeconds(0.5f);
+
         imgLike.transform.DOScale(1f, 0.5f);
         particleLike.Play();
         //Play Particle System
+        
         yield return new WaitForSeconds(0.5f);
+        
         GenerateComment();
         yield return new WaitForSeconds(4f);
         NextLevel();
+    }
+    public IEnumerator IE_FirstLikeNum()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            numLike++;
+            codeLike.text = numLike.ToString();
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        for (int i = 0; i < 50; i++)
+        {
+            numLike++;
+            codeLike.text = numLike.ToString();
+            yield return new WaitForSeconds(0.04f);
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            numLike++;
+            codeLike.text = numLike.ToString();
+            yield return new WaitForSeconds(0.1f);
+        }
     }
 
     public void GenerateComment()
