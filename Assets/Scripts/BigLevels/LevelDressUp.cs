dd5c2f0bb732561a6e325176a070c3a8d45b7fe1
Erica1743 <yaozixin0613@gmail.com>
2023-02-11 10:53:48 -0800

    dressup

--- Assets/Scripts/BigLevels/LevelDressUp.cs
+++ Assets/Scripts/BigLevels/LevelDressUp.cs
@@ -1,16 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+
+public enum DressType
+{
+    dress1,
+    dress2,
+    None
+}
 public class LevelDressUp : LevelBasic
 {
+    public DressType currentType;
+    public GameObject objCharacter;
+    public List<Sprite> newCharacter;
+    public List<DragDress> listDragDress = new List<DragDress>();
     public DressSlot itemDressSlot;
 
     public override void Init(LevelManager parent)
     {
         base.Init(parent);
-
         itemDressSlot.Init(this);
+        InitialDresses();
+    }
+
+    
+    public void InitialDresses()
+    {
+        listDragDress[0].Init(DressType.dress1,this);
+        listDragDress[1].Init(DressType.dress2, this);
+    }
+ public void ChangeCharacterDress()
+    {
+       //change the character image here according to the input from DragDress;
+       if(currentType == DressType.dress1)
+        {
+            print("dress1");
+            //character's spriet change to newCharacter[0]
+        }else if (currentType == DressType.dress2)
+        {
+            print("dress1");
+            //character's spriet change to newCharacter[1]
+        }
+        else { }
+
+    }
+
+
+
+    public void SetCurrentDress(DressType type)
+    {
+        currentType = type;
     }
 
     public IEnumerator IE_DragGoalFinish()
--- Assets/Scripts/LevelDetail/DressUp/DragDress.cs
+++ Assets/Scripts/LevelDetail/DressUp/DragDress.cs
@@ -7,12 +7,36 @@ using DG.Tweening;
 
 public class DragDress : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler,IDropHandler
 {
+
+    private LevelDressUp parent;
+    public DressType dresstype;//send dragging type
     public Image imgDress;
     public RectTransform rtDress;
+
+    public void Init(DressType type,LevelDressUp parent)
+    {
+        this.parent = parent;
+       // this.dresstype = type;
+
+    }
+
     //drag the dresses
     public void OnBeginDrag(PointerEventData eventData) {
+       /*
+        if(gameObject.name == "clothes1")
+        {
+            dresstype = DressType.dress1;
+        }else if (gameObject.name == "clothes2") {
+            dresstype = DressType.dress2;
+        }
+        else { }
+        */
+        
         imgDress.raycastTarget = false;
-        imgDress.DOFade(0.2f, 0);
+        imgDress.DOFade(0.8f, 0);
+        parent.SetCurrentDress(dresstype);
+      
+
     }
     public void OnDrag(PointerEventData eventData)
     {
@@ -22,7 +46,9 @@ public class DragDress : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, 
     public void OnEndDrag(PointerEventData eventData)
     {
         imgDress.raycastTarget = true;
-        imgDress.DOFade(1f, 0);
+        imgDress.DOFade(0f, 0);
+
+
     }
     public void OnPointerDown(PointerEventData eventData)
     {
