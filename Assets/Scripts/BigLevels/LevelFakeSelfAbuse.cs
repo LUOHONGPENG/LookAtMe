fdeee9e9d4e1b9691863774399210f9357966aed
LUOHONGPENG <32034557+LUOHONGPENG@users.noreply.github.com>
2023-02-12 23:44:00 +0000

    DeleteOldAbuse

--- Assets/Scripts/BigLevels/LevelFakeSelfAbuse.cs
+++ Assets/Scripts/BigLevels/LevelFakeSelfAbuse.cs
@@ -5,12 +5,10 @@ using UnityEngine.UI;
 
 public class LevelFakeSelfAbuse : LevelBasic
 {
-    public GameObject pfLipstick;
-    public Transform tfLipstick;
+
     public List<ItemColDetect> listColDetect;
 
-    private LipstickManager activeLine;
-
+    //The timer that check whether all point covered
     private float timerCheckReachAllPoints = 0.2f;
     private bool isDrawDone = false;
 
@@ -55,46 +53,24 @@ public class LevelFakeSelfAbuse : LevelBasic
             }
         }
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            GameObject newLine = Instantiate(pfLipstick,tfLipstick);
-            activeLine = newLine.GetComponent<LipstickManager>();
-        }
 
-        if (Input.GetMouseButtonUp(0))
-        {
-            activeLine = null;
-        }
+/*        Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D[] hits = Physics2D.RaycastAll(worldPoint, Vector2.zero);
 
-        if(activeLine != null)
+        foreach (RaycastHit2D hit in hits)
         {
-            Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            RaycastHit2D[] hits = Physics2D.RaycastAll(worldPoint, Vector2.zero);
-
-            foreach(RaycastHit2D hit in hits)
+            if (hit.collider != null)
             {
-                if (hit.collider != null)
+                if (hit.collider.tag == "ColDetect")
                 {
-                    if (hit.collider.tag == "ColMask")
+                    ItemColDetect itemDetect = hit.collider.gameObject.GetComponent<ItemColDetect>();
+                    if (!itemDetect.isTouched)
                     {
-                        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                        activeLine.UpdateLine(mousePos);
-                    }
-
-                    if (hit.collider.tag == "ColDetect")
-                    {
-                        ItemColDetect itemDetect = hit.collider.gameObject.GetComponent<ItemColDetect>();
-                        if (!itemDetect.isTouched)
-                        {
-                            itemDetect.isTouched = true;
-                        }
+                        itemDetect.isTouched = true;
                     }
                 }
             }
-
-
-            //AssignScreenAsMask();
-        }
+        }*/
     }
 
     public bool CheckAllPointsTouched()
@@ -118,36 +94,5 @@ public class LevelFakeSelfAbuse : LevelBasic
         yield return new WaitForSeconds(2f);
         NextLevel();
     }
-
-
-
-    /*    public void AssignScreenAsMask()
-        {
-            int height = Screen.height;
-            int width = Screen.width;
-            int depth = 1;
-
-            RenderTexture renderTexture = new RenderTexture(width, height, depth);
-            Rect rect = new Rect(0, 0, width, height);
-            Texture2D texture = new Texture2D(width, height, TextureFormat.ARGB32, false);
-
-            GameManager.Instance.cameraMask.targetTexture = renderTexture;
-            GameManager.Instance.cameraMask.Render();
-
-            RenderTexture currentRenderTexture = RenderTexture.active;
-            RenderTexture.active = renderTexture;
-            texture.ReadPixels(rect, 0, 0);
-            texture.Apply();
-
-            GameManager.Instance.cameraMask.targetTexture = null;
-            RenderTexture.active = currentRenderTexture;
-            Destroy(renderTexture);
-
-            Sprite sprite = Sprite.Create(texture, rect, new Vector2(0.5f, 0.5f), Screen.height / 10);
-
-
-
-            Debug.Log("AssignScreenAsMask");
-        }*/
 
 }
